db7d25ab1b66fde82446f6e706c42f5ef58356e3
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-05-29 15:28:22 +0530

    Price converter mobile optimization

--- components/PriceConverter.js
+++ components/PriceConverter.js
@@ -161,45 +161,46 @@ const CMCpriceConverter = ({
         {fromSymbol} to {toSymbol} Converter
       </h2>
       <br />
-      <div className="flex items-center justify-between rounded-xl border border-gray-500/10 bg-[#171924] px-5 py-5">
-        <div>
-          <div className="flex">
-            <div className="avatar-container">
+      <div className="grid items-center gap-4 rounded-xl border border-gray-500/10 bg-[#171924] px-5 py-5 md:grid-cols-5">
+        <div className="md:col-span-2">
+          <div className="flex w-full justify-between">
+            <div className="flex gap-2">
               {fromLogo && fromLogo ? coinIcon() : <div></div>}
+              <div>
+                <p>{fromSymbol}</p>
+                <h4>{from}</h4>
+              </div>
             </div>
             &nbsp; &nbsp;
-            <div>
-              <p>{fromSymbol}</p>
-              <h4>{from}</h4>
-            </div>
+            <p className="text-3xl">1</p>
           </div>
         </div>
 
-        <div className="flex">
-          <p className="text-3xl">1</p>
-          &nbsp;&nbsp;
+        <div className="flex justify-center">
           <div>
             <Image alt="" src={converter} width={40} height={40} />
           </div>
-          &nbsp;&nbsp;
-          <div className="flex">
+        </div>
+
+        <div className="flex justify-between md:col-span-2">
+          <div className="flex gap-2">
             {toLogo}
-            &nbsp; &nbsp;
             <div>
               <p>{toSymbol}</p>
               <h4>{to}</h4>
             </div>
           </div>
+          <p className="text-3xl">${price}</p>
         </div>
-
-        <p className="text-3xl">${price}</p>
       </div>
 
-      <div
-        className="mt-5 ml-auto w-min cursor-pointer rounded-xl bg-[#1d4ed8] p-2 px-5 hover:opacity-60"
-        // onClick={convertCrypto}
-      >
-        Convert
+      <div className="flex w-full justify-center">
+        <div
+          className="mt-5 w-60 cursor-pointer rounded-xl bg-[#1d4ed8] p-2 px-5 text-center hover:opacity-60"
+          // onClick={convertCrypto}
+        >
+          Convert
+        </div>
       </div>
     </div>
   )
