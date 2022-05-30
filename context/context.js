90e31ff64ef65cf32372b91cd384dc0d8d3dc251
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-05-30 22:14:26 +0530

    Context bug fix

--- context/context.js
+++ context/context.js
@@ -15,126 +15,128 @@ import {
 
 export const CoinMarketContext = createContext()
 
-const { isAuthenticated, user, Moralis } = useMoralis()
+export const CoinMarketProvider = ({ children }) => {
+  const { isAuthenticated, user, Moralis } = useMoralis()
+  const [currentAccount, setCurrentAccount] = useState('')
+  const [openBuyCryptoModal, setOpenBuyCryptoModal] = useState(false)
+  const [fromToken, setFromToken] = useState('ETH')
+  const [toToken, setToToken] = useState('')
+  const [amount, setAmount] = useState('')
 
-const [currentAccount, setCurrentAccount] = useState('')
-const [openBuyCryptoModal, setOpenBuyCryptoModal] = useState(false)
-const [fromToken, setFromToken] = useState('ETH')
-const [toToken, setToToken] = useState('')
-const [amount, setAmount] = useState('')
+  useEffect(() => {
+    if (isAuthenticated) {
+      const account = user.get('ethAddress')
+      setCurrentAccount(account)
+    }
+  }, [isAuthenticated])
 
-const { data: coins, error, isLoading: loadingCoins } = useMoralisQuery('Coins')
-
-useEffect(() => {
-  if (isAuthenticated) {
-    const account = user.get('ethAddress')
-    setCurrentAccount(account)
+  const getContractAddress = () => {
+    if (fromToken === 'Dai') return daiAddress
+    if (fromToken === 'Dogecoin') return dogeAddress
+    if (fromToken === 'Link') return linkAddress
+    if (fromToken === 'usdc') return usdcAddress
   }
-}, [isAuthenticated])
 
-const getContractAddress = () => {
-  if (fromToken === 'Dai') return daiAddress
-  if (fromToken === 'Dogecoin') return dogeAddress
-  if (fromToken === 'Link') return linkAddress
-  if (fromToken === 'usdc') return usdcAddress
-}
+  const getToAddress = () => {
+    if (toToken === 'Dai') return daiAddress
+    if (toToken === 'Dogecoin') return dogeAddress
+    if (toToken === 'Link') return linkAddress
+    if (toToken === 'Usdc') return usdcAddress
+  }
 
-const getToAddress = () => {
-  if (toToken === 'Dai') return daiAddress
-  if (toToken === 'Dogecoin') return dogeAddress
-  if (toToken === 'Link') return linkAddress
-  if (toToken === 'Usdc') return usdcAddress
-}
+  const getToAbi = () => {
+    if (toToken === 'Dai') return daiAbi
+    if (toToken === 'Dogecoin') return dogeAbi
+    if (toToken === 'Link') return linkAbi
+    if (toToken === 'Usdc') return usdcAbi
+  }
 
-const getToAbi = () => {
-  if (toToken === 'Dai') return daiAbi
-  if (toToken === 'Dogecoin') return dogeAbi
-  if (toToken === 'Link') return linkAbi
-  if (toToken === 'Usdc') return usdcAbi
-}
+  const openModal = () => {
+    setOpenBuyCryptoModal(true)
+  }
 
-const openModal = () => {
-  setOpenBuyCryptoModal(true)
-}
+  //Mint function for the token with send ether to the contract
+  const mint = async () => {
+    try {
+      if (fromToken === 'ETH') {
+        if (!isAuthenticated) return
+        await Moralis.enableWeb3()
+        const contractAddress = getToAddress()
+        const abi = getToAbi()
 
-//Mint function for the token with send ether to the contract
-const mint = async () => {
-  try {
-    if (fromToken === 'ETH') {
+        let options = {
+          contractAddress: contractAddress,
+          functionName: 'mint',
+          abi: abi,
+          params: {
+            to: currentAccount,
+            amount: Moralis.Units.Token(amount),
+          },
+        }
+        sendEth()
+        const transaction = await Moralis.executeFunction(options)
+        const receipt = await transaction.wait(4)
+        console.log(receipt)
+      } else {
+        swapTokens()
+      }
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  const sendEth = async () => {
+    if (!isAuthenticated) return
+    const contractAddress = getToAddress()
+
+    let options = {
+      type: 'native',
+      amount: Moralis.Units.ETH('0.01'),
+      receiver: contractAddress,
+    }
+    const transaction = await Moralis.transfer(options)
+    const receipt = await transaction.wait()
+    console.log(receipt)
+  }
+
+  const swapTokens = async () => {
+    try {
       if (!isAuthenticated) return
       await Moralis.enableWeb3()
-      const contractAddress = getToAddress()
-      const abi = getToAbi()
 
-      let options = {
-        contractAddress: contractAddress,
+      if (fromToken === toToken) return alert('You cannot swap the same token')
+
+      const fromOptions = {
+        type: 'erc20',
+        amount: Moralis.Units.Token(amount, '18'),
+        receiver: getContractAddress(),
+        contractAddress: getContractAddress(),
+      }
+      const toMintOptions = {
+        contractAddress: getToAddress(),
         functionName: 'mint',
-        abi: abi,
+        abi: getToAbi(),
         params: {
           to: currentAccount,
-          amount: Moralis.Units.Token(amount),
+          amount: Moralis.Units.Token(amount, '18'),
         },
       }
-      sendEth()
-      const transaction = await Moralis.executeFunction(options)
-      const receipt = await transaction.wait(4)
-      console.log(receipt)
-    } else {
-      swapTokens()
+      let fromTransaction = await Moralis.transfer(fromOptions)
+      let toMintTransaction = await Moralis.executeFunction(toMintOptions)
+      let fromReceipt = await fromTransaction.wait()
+      let toReceipt = await toMintTransaction.wait()
+      console.log(fromReceipt)
+      console.log(toReceipt)
+    } catch (error) {
+      console.error(error.message)
     }
-  } catch (error) {
-    console.error(error.message)
   }
-}
 
-const sendEth = async () => {
-  if (!isAuthenticated) return
-  const contractAddress = getToAddress()
-
-  let options = {
-    type: 'native',
-    amount: Moralis.Units.ETH('0.01'),
-    receiver: contractAddress,
-  }
-  const transaction = await Moralis.transfer(options)
-  const receipt = await transaction.wait()
-  console.log(receipt)
-}
-
-const swapTokens = async () => {
-  try {
-    if (!isAuthenticated) return
-    await Moralis.enableWeb3()
-
-    if (fromToken === toToken) return alert('You cannot swap the same token')
-
-    const fromOptions = {
-      type: 'erc20',
-      amount: Moralis.Units.Token(amount, '18'),
-      receiver: getContractAddress(),
-      contractAddress: getContractAddress(),
-    }
-    const toMintOptions = {
-      contractAddress: getToAddress(),
-      functionName: 'mint',
-      abi: getToAbi(),
-      params: {
-        to: currentAccount,
-        amount: Moralis.Units.Token(amount, '18'),
-      },
-    }
-    let fromTransaction = await Moralis.transfer(fromOptions)
-    let toMintTransaction = await Moralis.executeFunction(toMintOptions)
-    let fromReceipt = await fromTransaction.wait()
-    let toReceipt = await toMintTransaction.wait()
-    console.log(fromReceipt)
-    console.log(toReceipt)
-  } catch (error) {
-    console.error(error.message)
-  }
-}
-
-export const CoinMarketProvider = ({ children }) => {
+  const {
+    data: coins,
+    error,
+    isLoading: loadingCoins,
+  } = useMoralisQuery('Coins')
   const getTopTenCoins = async () => {
     try {
       const res = await fetch('/api/getTopTen')
