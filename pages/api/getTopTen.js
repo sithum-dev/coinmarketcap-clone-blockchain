c863826a33e11227198244db93a6203c9dd1cdac
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-06-02 14:02:38 +0530

    API req bug fix

--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -1,20 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default function handler(req, res) {
-  const getData = async () => {
-    const response = await fetch(
-      `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.NEXT_BACKEND_CMC_API_KEY}`,
-      {
-        method: 'GET',
-        headers: {
-          Accept: '*/*',
-        },
-      }
-    )
+  try {
+    const getData = async () => {
+      const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.NEXT_BACKEND_CMC_API_KEY}`,
+        {
+          method: 'GET',
+          headers: {
+            Accept: '*/*',
+          },
+        }
+      )
 
-    const data = await response.json()
+      const data = await response.json()
 
-    res.status(200).json({ data })
+      res.status(200).json({ data })
+    }
+
+    getData()
+  } catch (error) {
+    console.log(error)
   }
-
-  getData()
 }
