e88af644ee5d428c551b35f26b4f1da9b90637de
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-06-02 14:12:53 +0530

    more button UI update

--- components/MoreButton.js
+++ components/MoreButton.js
@@ -2,8 +2,8 @@ import { ChevronRightIcon } from '@heroicons/react/solid'
 
 const MoreButton = () => {
   return (
-    <div className="flex cursor-pointer items-center justify-between whitespace-nowrap text-[#6188FF]">
-      More <ChevronRightIcon />
+    <div className="flex cursor-pointer items-center justify-center align-middle text-[#6188FF]">
+      <ChevronRightIcon className="h-5 w-5" />
     </div>
   )
 }
