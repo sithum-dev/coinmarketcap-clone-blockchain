d3d281ef5ff63343144eee569fa2814ba759df67
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-06-05 17:32:36 +0530

    Responsive bug fix

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 const PostCard = ({ banner, category, title }) => {
   return (
-    <div className="relative">
-      <Image src={banner} objectFit="cover" className="rounded-md" />
+    <div className="">
+      <Image src={banner} className="h-20 w-96 rounded-md" />
       <div>
         <p className="py-1 text-xs text-gray-300">{category}</p>
         <p className="truncate text-sm text-white ">{title}</p>
